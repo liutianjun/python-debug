f9d9964cda6e37d30cd28e39457c113b67f23c3c
liutianjun <474307850@qq.com>
2021-03-31 09:23:20 +0800

    添加调试打印信息日期时间、文件名行号、函数名

--- codec/codec.py
+++ codec/codec.py
@@ -1,11 +1,14 @@
 import sys
+import time
+import os
 
 s = '\xd6\xc2\xc3\xfc\xb4\xed\xce\xf3: \xca\xfd\xbe\xe2 \"bbs-go\" \xb2\xbb\xb4\xe6\xd4\xda (SQLSTATE 3D000)'
 
 
 def decodeStr():
     ss = s.encode('raw_unicode_escape').decode('gbk')
-    print(str(sys._getframe().f_lineno) + "行: " + ss)
+    print(time.strftime('%Y/%m/%d %H:%M:%S ', time.localtime(time.time())) + __file__ + ":" + str(
+        sys._getframe().f_lineno) + " 函数名:" + str(sys._getframe().f_code.co_name) + "(): " + ss)
 
 
 if __name__ == '__main__':
